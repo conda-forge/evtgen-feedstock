5eeb20be7fb4f661c5a27302e6401eb306ace99a
Chris Burr <christopher.burr@cern.ch>
2021-05-21 12:34:30 +0200

    Enable linking to PHOTOS and TAUOLA

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,8 +7,8 @@ cmake \
     -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
     -DEVTGEN_HEPMC3=ON \
     -DEVTGEN_PYTHIA=ON \
-    -DEVTGEN_PHOTOS=OFF \
-    -DEVTGEN_TAUOLA=OFF \
+    -DEVTGEN_PHOTOS=ON \
+    -DEVTGEN_TAUOLA=ON \
     ../R0*/
 
 cmake --build . --target install "-j${CPU_COUNT}"
